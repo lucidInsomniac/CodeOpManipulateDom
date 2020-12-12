6a6f0d9ed152d0534abc8023e4ea801b260a0f96
DonnaC <donnchin@gmail.com>
2020-12-12 14:56:35 -0500

    almost done in part 3, but image won't display in feature

--- index.js
+++ index.js
@@ -1,24 +1,75 @@
-const button = window.document.getElementById("button");
+/*******************************Global Variabes*******************************************/
+let AllImages = {}
+let NextImageId= 1;
 
-function handleClick(event) {
+
+/*******************************Event Handlers********************************************/
+function handleSubmit(event) {
   //prevents data from going to server
   event.preventDefault();
 
-  //Get values from form
-  let input = window.document.getElementById("imageURL").value;
-  console.log("button was pressed")
-  console.log(input);        
+  //Get values from form ("imageURL" and "descr") and reset fields, reset empties out form
+  let form = event.target;
+  let portTitle = form.elements.portTitle.value;
+  let imageURL = window.document.getElementById("imageURL").value;
+  let descr = form.elements.descr.value;
+      console.log("button was pressed");
+      console.log(imageURL);
+  //empties out form
+  form.reset();
+  
 
+  // Save data (imageURL and descr) to global obj variables (AllImages, and NextImageId)
+  let imageId = NextImageId++;
+  AllImages[imageId] = {portTitle: portTitle, imageURL: imageURL, descr: descr}
+
+
+  // Create image with text and background image from form
   // creating a new image, new var not from HTML
-  let img =window.document.createElement('img');
+  let img = window.document.createElement('img');
+  img.classList.add('img');
+  img.innerText = imageURL
+  //CSS "background-image"
+  img.style.backgroundColor = imageURL;
+  img.id = imageId
 
-  // Store input data inside the new var, img.src
-  img.src = input;
-  document.getElementById('imagesUp').appendChild(img);
-  document.getElementById('form').reset();
+  //Append to my "image-grid" container
+  img.src = imageURL;
+  let grid = document.getElementById('image-grid');
+  grid.appendChild(img);
 
 }
 
-button.addEventListener("click", handleClick);
+//action when user clicks on image cells on container
+function handleClick(event) {
+  //Ignores clicks that are not on image/cell
+  if (event.target.id === "image-grid" ){
+    return;
+  }
+
+  //Get corresponding  data for this image
+  let imageId = event.target.id;
+  let data = AllImages[imageId];
+
+  //Show data in featured image section
+  let featImage = document.getElementById('feat-image');
+  featImage.textContext = data.imageURL
+  featImage.style.backgroundColor= data.imageURL;
+  document.getElementById('feat-descr').textContent = data.descr
+
+  //Make sure featured image section is visible
+  document.getElementById('feat-section').style.display = 'block';
+}
+
+
+/*******************************Initialization********************************************/
+
+//Tell form to call handleSubmit() on "submit" events
+let form = document.querySelector('form')
+form.addEventListener("submit", handleSubmit);
+
+// //Tell image-grid to call on handeClick() on "click" events
+let grid =  document.getElementById('image-grid');
+grid.addEventListener('click', handleClick);
 
 
